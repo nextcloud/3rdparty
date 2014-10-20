nagyist/owncloud-3rdparty

Adding punic/punic 1.1.0

autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit96654a5746889b254811c7316da366d7::getLoader();
+return ComposerAutoloaderInit0b411b80e6b5acee4fb96b44c1f91e2d::getLoader();

nextcloud/3rdparty

autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3669affcbbaeb5ccc4a88f42b228548d::getLoader();
+return ComposerAutoloaderInit62842db033d4441fd978715211e15a55::getLoader();