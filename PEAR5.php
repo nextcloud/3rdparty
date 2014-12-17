nextcloud/3rdparty

commit d4128b3d38ce0405bbfa2b1cca0bbc16b8698f57

composer update pear/archive_tar

PEAR5.php
@@ -1,33 +1,0 @@
-<?php
-/**
- * This is only meant for PHP 5 to get rid of certain strict warning
- * that doesn't get hidden since it's in the shutdown function
- */
-class PEAR5
-{
-    /**
-    * If you have a class that's mostly/entirely static, and you need static
-    * properties, you can use this method to simulate them. Eg. in your method(s)
-    * do this: $myVar = &PEAR5::getStaticProperty('myclass', 'myVar');
-    * You MUST use a reference, or they will not persist!
-    *
-    * @access public
-    * @param  string $class  The calling classname, to prevent clashes
-    * @param  string $var    The variable to retrieve.
-    * @return mixed   A reference to the variable. If not set it will be
-    *                 auto initialised to NULL.
-    */
-    static function &getStaticProperty($class, $var)
-    {
-        static $properties;
-        if (!isset($properties[$class])) {
-            $properties[$class] = array();
-        }
-
-        if (!array_key_exists($var, $properties[$class])) {
-            $properties[$class][$var] = null;
-        }
-
-        return $properties[$class][$var];
-    }
-}

autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2d5e2a6786924804304fd7b7eaccd311::getLoader();
+return ComposerAutoloaderInit1fae751d8dfdc424837f4fa88ab08cb9::getLoader();

composer/autoload_namespaces.php
@@ -18,6 +18,7 @@ return array(
     'Sabre\\CalDAV' => array($vendorDir . '/sabre/dav/lib'),
     'RandomLib' => array($vendorDir . '/ircmaxell/random-lib/lib'),
     'Patchwork' => array($vendorDir . '/patchwork/utf8/class'),
+    'PEAR' => array($vendorDir . '/pear/pear_exception'),
     'OpenCloud' => array($vendorDir . '/rackspace/php-opencloud/lib', $vendorDir . '/rackspace/php-opencloud/tests'),
     'Normalizer' => array($vendorDir . '/patchwork/utf8/class'),
     'Net' => array($vendorDir . '/phpseclib/phpseclib/phpseclib'),
@@ -36,4 +37,5 @@ return array(
     'Doctrine\\Common\\' => array($vendorDir . '/doctrine/common/lib'),
     'Crypt' => array($vendorDir . '/phpseclib/phpseclib/phpseclib'),
     'Assetic' => array($vendorDir . '/kriswallsmith/assetic/src'),
+    'Archive_Tar' => array($vendorDir . '/pear/archive_tar'),
 );

composer/autoload_real.php
@@ -2,7 +2,7 @@
 
 // autoload_real.php @generated by Composer
 
-class ComposerAutoloaderInit2d5e2a6786924804304fd7b7eaccd311
+class ComposerAutoloaderInit1fae751d8dfdc424837f4fa88ab08cb9
 {
     private static $loader;
 
@@ -19,9 +19,9 @@ class ComposerAutoloaderInit2d5e2a6786924804304fd7b7eaccd311
             return self::$loader;
         }
 
-        spl_autoload_register(array('ComposerAutoloaderInit2d5e2a6786924804304fd7b7eaccd311', 'loadClassLoader'), true, true);
+        spl_autoload_register(array('ComposerAutoloaderInit1fae751d8dfdc424837f4fa88ab08cb9', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
-        spl_autoload_unregister(array('ComposerAutoloaderInit2d5e2a6786924804304fd7b7eaccd311', 'loadClassLoader'));
+        spl_autoload_unregister(array('ComposerAutoloaderInit1fae751d8dfdc424837f4fa88ab08cb9', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
         array_push($includePaths, get_include_path());
@@ -46,14 +46,14 @@ class ComposerAutoloaderInit2d5e2a6786924804304fd7b7eaccd311
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
         foreach ($includeFiles as $file) {
-            composerRequire2d5e2a6786924804304fd7b7eaccd311($file);
+            composerRequire1fae751d8dfdc424837f4fa88ab08cb9($file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire2d5e2a6786924804304fd7b7eaccd311($file)
+function composerRequire1fae751d8dfdc424837f4fa88ab08cb9($file)
 {
     require $file;
 }

composer/include_paths.php
@@ -7,4 +7,6 @@ $baseDir = $vendorDir;
 
 return array(
     $vendorDir . '/phpseclib/phpseclib/phpseclib',
+    $vendorDir . '/pear/pear_exception',
+    $vendorDir . '/pear/archive_tar',
 );

pear/archive_tar/.gitignore
@@ -1,0 +1,6 @@
+# composer related
+composer.lock
+composer.phar
+vendor
+# IDE
+.idea

pear/archive_tar/Archive/Tar.php
@@ -47,21 +47,21 @@ define('ARCHIVE_TAR_END_BLOCK', pack("a512", ''));
 if (!function_exists('gzopen') && function_exists('gzopen64')) {
     function gzopen($filename, $mode, $use_include_path = 0)
     {
-        gzopen64($filename, $mode, $use_include_path);
+        return gzopen64($filename, $mode, $use_include_path);
     }
 }
 
 if (!function_exists('gztell') && function_exists('gztell64')) {
     function gztell($zp)
     {
-        gztell64($zp);
+        return gztell64($zp);
     }
 }
 
 if (!function_exists('gzseek') && function_exists('gzseek64')) {
     function gzseek($zp, $offset, $whence = SEEK_SET)
     {
-        gzseek64($zp, $offset, $whence);
+        return gzseek64($zp, $offset, $whence);
     }
 }
 

commit f5237549e12189848608d23d34fea3b64b0131a1

Add PEAR5.php back.
Follows 68ad46637824fd4e27ff1d07d31e83ae747f8581
Regression from d4128b3d38ce0405bbfa2b1cca0bbc16b8698f57

PEAR5.php
@@ -1,0 +1,33 @@
+<?php
+/**
+ * This is only meant for PHP 5 to get rid of certain strict warning
+ * that doesn't get hidden since it's in the shutdown function
+ */
+class PEAR5
+{
+    /**
+    * If you have a class that's mostly/entirely static, and you need static
+    * properties, you can use this method to simulate them. Eg. in your method(s)
+    * do this: $myVar = &PEAR5::getStaticProperty('myclass', 'myVar');
+    * You MUST use a reference, or they will not persist!
+    *
+    * @access public
+    * @param  string $class  The calling classname, to prevent clashes
+    * @param  string $var    The variable to retrieve.
+    * @return mixed   A reference to the variable. If not set it will be
+    *                 auto initialised to NULL.
+    */
+    static function &getStaticProperty($class, $var)
+    {
+        static $properties;
+        if (!isset($properties[$class])) {
+            $properties[$class] = array();
+        }
+
+        if (!array_key_exists($var, $properties[$class])) {
+            $properties[$class][$var] = null;
+        }
+
+        return $properties[$class][$var];
+    }
+}