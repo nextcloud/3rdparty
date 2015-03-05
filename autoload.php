metaworx/oc_3rdparty

add generic stream wrapper developed by icewind, it is already in use for the samba back-end and will be used for encryption

autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd2d696763331b6ad4ab1adcd2e35403a::getLoader();
+return ComposerAutoloaderInit24cba19ee70f2bb7987882dde864c5c5::getLoader();

nextcloud/3rdparty

autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf8f4b6b0052169585e376266efaa5716::getLoader();
+return ComposerAutoloaderInit534f628906b03cd0188ec46eb2dc1042::getLoader();