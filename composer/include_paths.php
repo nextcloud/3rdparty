daacb34dc7db42165e2ec8262b1bb800d35e7984
Thomas Müller <thomas.mueller@tmit.eu>
2015-08-05 11:42:46 +0200

    "pear/pear-core-minimal": "v1.10.0alpha2"

--- composer/include_paths.php
+++ composer/include_paths.php
@@ -6,9 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = $vendorDir;
 
 return array(
-    $vendorDir . '/pear/pear_exception',
     $vendorDir . '/phpseclib/phpseclib/phpseclib',
     $vendorDir . '/pear/console_getopt',
-    $vendorDir . '/pear/pear-core-minimal/src',
     $vendorDir . '/pear/archive_tar',
+    $vendorDir . '/pear/pear_exception',
+    $vendorDir . '/pear/pear-core-minimal/src',
 );
