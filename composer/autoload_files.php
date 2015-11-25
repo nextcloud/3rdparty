8603194c66b438aa57224ce7852d27b12e95e881
Roeland Jago Douma <rullzer@owncloud.com>
2015-11-25 22:23:28 +0100

    Bump symfony routing to 2.7.7

--- composer/autoload_files.php
+++ composer/autoload_files.php
@@ -6,11 +6,11 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = $vendorDir;
 
 return array(
-    '651f6769966572fcfbc44ed112b4490e' => $vendorDir . '/sabre/uri/lib/functions.php',
-    '3ad337fb1e17055256ca59753bdfe795' => $vendorDir . '/react/promise/src/functions_include.php',
-    '742cc5b936ec765a3455c0a976796f29' => $vendorDir . '/nikic/php-parser/lib/bootstrap.php',
-    'a7f9cd98d7a35ae00ec75fb6117e0593' => $vendorDir . '/sabre/http/lib/functions.php',
-    '01b872096dad055849387e42d41a176d' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
-    '05669301f06448944ce5eb260126f84e' => $vendorDir . '/ircmaxell/password-compat/lib/password.php',
-    'f0cfed1a23b444183024a3c7df93aafa' => $vendorDir . '/kriswallsmith/assetic/src/functions.php',
+    $vendorDir . '/sabre/uri/lib/functions.php',
+    $vendorDir . '/react/promise/src/functions_include.php',
+    $vendorDir . '/nikic/php-parser/lib/bootstrap.php',
+    $vendorDir . '/sabre/http/lib/functions.php',
+    $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
+    $vendorDir . '/ircmaxell/password-compat/lib/password.php',
+    $vendorDir . '/kriswallsmith/assetic/src/functions.php',
 );
