26e4b6da7fc90364585b3534d902f0cd41536d2a
Lukas Reschke <lukas@owncloud.com>
2016-04-04 12:19:26 +0200

    Remove ircmaxell/random-lib
    Not used anymore since 9.0

--- composer/autoload_namespaces.php
+++ composer/autoload_namespaces.php
@@ -6,8 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = $vendorDir;
 
 return array(
-    'SecurityLib' => array($vendorDir . '/ircmaxell/security-lib/lib'),
-    'RandomLib' => array($vendorDir . '/ircmaxell/random-lib/lib'),
     'Pimple' => array($vendorDir . '/pimple/pimple/src'),
     'PEAR' => array($vendorDir . '/pear/pear_exception'),
     'OpenCloud' => array($vendorDir . '/rackspace/php-opencloud/lib', $vendorDir . '/rackspace/php-opencloud/tests'),
