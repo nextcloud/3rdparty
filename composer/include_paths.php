7ca948cc0ff903e3ecd03615632733c1bd5d74cf
Lukas Reschke <lukas@statuscode.ch>
2017-07-19 17:21:44 +0200

    Bump Archive_Tar from 1.4.1 to 1.4.3
    Required for https://github.com/nextcloud/server/pull/5791
    
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- composer/include_paths.php
+++ composer/include_paths.php
@@ -8,6 +8,6 @@ $baseDir = $vendorDir;
 return array(
     $vendorDir . '/pear/console_getopt',
     $vendorDir . '/pear/pear_exception',
-    $vendorDir . '/pear/archive_tar',
     $vendorDir . '/pear/pear-core-minimal/src',
+    $vendorDir . '/pear/archive_tar',
 );
