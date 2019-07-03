22b96ea4dfb257668fcda60de869df4770567559
Alexander Ivannikov <alexander.ivannikov@dsr-company.com>
2019-07-03 18:57:01 +0300

    Set DTSTAMP to NOW for iTip message in case of event update or cancellation
    Signed-off-by: Alexander Ivannikov <alexander.ivannikov@dsr-company.com>

--- sabre/vobject/lib/ITip/Broker.php
+++ sabre/vobject/lib/ITip/Broker.php
@@ -531,6 +531,7 @@ class Broker {
                 $event = $icalMsg->add('VEVENT', [
                     'UID'      => $message->uid,
                     'SEQUENCE' => $message->sequence,
+                    'DTSTAMP' => gmdate('Ymd\\THis\\Z'),
                 ]);
                 if (isset($calendar->VEVENT->SUMMARY)) {
                     $event->add('SUMMARY', $calendar->VEVENT->SUMMARY->getValue());
@@ -619,7 +620,7 @@ class Broker {
                         }
 
                     }
-
+                    $currentEvent->DTSTAMP = gmdate('Ymd\\THis\\Z');
                     $icalMsg->add($currentEvent);
 
                 }
