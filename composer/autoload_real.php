f7b86b6e3abc0804de9c281003172f67960392f4
Roeland Jago Douma <roeland@famdouma.nl>
2020-03-30 09:38:53 +0200

    Add webauthn lib
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- composer/autoload_real.php
+++ composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit2f23f73bc0cc116b4b1eee1521aa8652
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
