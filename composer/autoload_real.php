f6c04235968b97347c2b69d010e53814ab0c51aa
Vincent Petry <vincent@nextcloud.com>
2021-10-18 15:04:21 +0200

    Bump icewind/streams from 0.7.1 to 0.7.5
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>

--- composer/autoload_real.php
+++ composer/autoload_real.php
@@ -22,10 +22,8 @@ class ComposerAutoloaderInit2f23f73bc0cc116b4b1eee1521aa8652
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit2f23f73bc0cc116b4b1eee1521aa8652', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2f23f73bc0cc116b4b1eee1521aa8652', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
@@ -34,7 +32,7 @@ class ComposerAutoloaderInit2f23f73bc0cc116b4b1eee1521aa8652
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit2f23f73bc0cc116b4b1eee1521aa8652::getInitializer($loader));
         } else {
