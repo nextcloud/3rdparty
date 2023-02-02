070519405b55684c5c48941a6f63cfe3d7d21999
Joas Schilling <coding@schilljs.com>
2023-02-02 06:48:15 +0100

    Bump guzzlehttp/guzzle and guzzlehttp/psr7
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- composer/include_paths.php
+++ composer/include_paths.php
@@ -6,8 +6,8 @@ $vendorDir = dirname(__DIR__);
 $baseDir = $vendorDir;
 
 return array(
-    $vendorDir . '/pear/pear_exception',
+    $vendorDir . '/pear/archive_tar',
     $vendorDir . '/pear/console_getopt',
     $vendorDir . '/pear/pear-core-minimal/src',
-    $vendorDir . '/pear/archive_tar',
+    $vendorDir . '/pear/pear_exception',
 );
