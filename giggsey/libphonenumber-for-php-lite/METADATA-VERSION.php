28c6113f858251cf6344435678a7044238aad4c3
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-09-25 05:53:11 +0000

    chore(autoloader): Dump autoloader
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ giggsey/libphonenumber-for-php-lite/METADATA-VERSION.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file specifies the revision of the metadata to build from.
+ * It can be a commit, branch or tag of the https://github.com/google/libphonenumber project
+ * For more information, look at the phing tasks in build.xml
+ * @internal
+ */
+return 'v8.13.45';
---
+++ giggsey/libphonenumber-for-php-lite/src/data/ShortNumberMetadata_UY.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * libphonenumber-for-php-lite data file
+ * This file has been @generated from libphonenumber data
+ * Do not modify!
+ * @internal
+ */
+
+return [
+    'id' => 'UY',
+    'countryCode' => 0,
+    'generalDesc' => [
+        'pattern' => '[19]\\d{2,3}',
+        'posLength' => [
+            3,
+            4,
+        ],
+    ],
+    'tollFree' => [
+        'pattern' => '128|911',
+        'example' => '128',
+        'posLength' => [
+            3,
+        ],
+    ],
+    'premiumRate' => [
+        'posLength' => [
+            -1,
+        ],
+    ],
+    'emergency' => [
+        'pattern' => '128|911',
+        'example' => '128',
+        'posLength' => [
+            3,
+        ],
+    ],
+    'shortCode' => [
+        'pattern' => '1(?:0[4-9]|1[2368]|2[0-3568]|787)|911',
+        'example' => '104',
+    ],
+    'standardRate' => [
+        'pattern' => '178\\d',
+        'example' => '1780',
+        'posLength' => [
+            4,
+        ],
+    ],
+    'carrierSpecific' => [
+        'posLength' => [
+            -1,
+        ],
+    ],
+    'smsServices' => [
+        'posLength' => [
+            -1,
+        ],
+    ],
+    'internationalPrefix' => '',
+    'numberFormat' => [],
+];
