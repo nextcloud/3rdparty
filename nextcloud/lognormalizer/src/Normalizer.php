6122fa0fc672a150cabd23b0def261e51a915052
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-10-24 13:58:07 +0000

    chore(autoloader): Dump autoloader
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- nextcloud/lognormalizer/src/Normalizer.php
+++ nextcloud/lognormalizer/src/Normalizer.php
@@ -1,52 +1,52 @@
 <?php
+
+declare(strict_types=1);
+
 /**
- * interfaSys - lognormalizer
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- * @author Olivier Paroz <dev-lognormalizer@interfasys.ch>
- * @author Jordi Boggiano <j.boggiano@seld.be>
- *
- * @copyright Olivier Paroz 2015
- * @copyright Jordi Boggiano 2014-2015
+ * SPDX-FileCopyrightText: 2016-2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2015 Olivier Paroz <dev-lognormalizer@interfasys.ch>
+ * SPDX-FileCopyrightText: 2014-2015 Jordi Boggiano <j.boggiano@seld.be>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 namespace Nextcloud\LogNormalizer;
 
 use Throwable;
+use function is_float;
+use function is_scalar;
 
 /**
- * Converts any variable to a String
+ * Converts any variable to a string
  *
  * @package Nextcloud\LogNormalizer
+ * @api
  */
 class Normalizer {
 
 	/**
 	 * @type string
 	 */
-	const SIMPLE_DATE = "Y-m-d H:i:s";
+	private const SIMPLE_DATE = 'Y-m-d H:i:s';
 
 	/**
-	 * @type int
+	 * @var int
 	 */
 	private $maxRecursionDepth;
 
 	/**
-	 * @type int
+	 * @var int
 	 */
 	private $maxArrayItems;
 
 	/**
-	 * @type string
+	 * @var string
 	 */
 	private $dateFormat;
 
 	/**
 	 * @param int $maxRecursionDepth The maximum depth at which to go when inspecting objects
 	 * @param int $maxArrayItems The maximum number of Array elements you want to show, when
-	 *     parsing an array
+	 *                           parsing an array
 	 * @param null|string $dateFormat The format to apply to dates
 	 */
 	public function __construct($maxRecursionDepth = 4, $maxArrayItems = 20, $dateFormat = null) {
@@ -60,42 +60,40 @@ class Normalizer {
 	}
 
 	/**
-	 * Normalises the variable, JSON encodes it if needed and cleans up the result
+	 * Normalizes the variable, JSON encodes it if needed and cleans up the result
 	 *
 	 * @param mixed $data
-	 *
 	 * @return string|null
 	 */
-	public function format(&$data) {
+	#[\NoDiscard]
+	public function format($data): ?string {
 		$data = $this->normalize($data);
-		$data = $this->convertToString($data);
-
-		return $data;
+		return $this->convertToString($data);
 	}
 
 	/**
-	 * Converts Objects, Arrays, Dates and Exceptions to a String or an Array
+	 * Converts Objects, Arrays, Dates and Exceptions to a string or an Array
 	 *
 	 * @uses Nextcloud\LogNormalizer\Normalizer::normalizeTraversable
 	 * @uses Nextcloud\LogNormalizer\Normalizer::normalizeDate
 	 * @uses Nextcloud\LogNormalizer\Normalizer::normalizeObject
 	 * @uses Nextcloud\LogNormalizer\Normalizer::normalizeResource
 	 *
-	 * @param $data
+	 * @param mixed $data
 	 * @param int $depth
 	 *
-	 * @return string|array
+	 * @return mixed|array
 	 */
-	public function normalize($data, $depth = 0) {
+	public function normalize($data, ?int $depth = 0) {
 		$scalar = $this->normalizeScalar($data);
-		if (!is_array($scalar)) {
+		if ($scalar !== null) {
 			return $scalar;
 		}
 		$decisionArray = [
 			'normalizeTraversable' => [$data, $depth],
-			'normalizeDate'        => [$data],
-			'normalizeObject'      => [$data, $depth],
-			'normalizeResource'    => [$data],
+			'normalizeDate' => [$data],
+			'normalizeObject' => [$data, $depth],
+			'normalizeResource' => [$data],
 		];
 
 		foreach ($decisionArray as $functionName => $arguments) {
@@ -117,11 +115,11 @@ class Normalizer {
 	 *
 	 * @return string|null
 	 */
-	public function convertToString($data) {
+	public function convertToString($data): ?string {
 		if (!is_string($data)) {
-			$data = @json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+			$data = @json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PARTIAL_OUTPUT_ON_ERROR);
 			// Removing null byte and double slashes from object properties
-			$data = str_replace(['\\u0000', '\\\\'], ["", "\\"], $data);
+			$data = str_replace(['\\u0000', '\\\\'], ['', '\\'], $data);
 		}
 
 		return $data;
@@ -132,45 +130,44 @@ class Normalizer {
 	 *
 	 * We're returning an array here to detect failure because null is a scalar and so is false
 	 *
-	 * @param $data
+	 * @param mixed $data
 	 *
-	 * @return mixed
+	 * @return string|mixed|null
 	 */
 	private function normalizeScalar($data) {
-		if (null === $data || is_scalar($data)) {
-			/*// utf8_encode only works for Latin1 so we rely on mbstring
-			if (is_string($data)) {
-				$data = mb_convert_encoding($data, "UTF-8");
-			}*/
+		if ($data === null) {
+			return null;
+		}
 
-			if (is_float($data)) {
-				$data = $this->normalizeFloat($data);
-			}
+		if (is_float($data)) {
+			return $this->normalizeFloat($data);
+		}
 
+		if (is_scalar($data)) {
 			return $data;
 		}
 
-		return [];
+		return null;
 	}
 
 	/**
-	 * Normalises infinite and trigonometric floats
+	 * Normalizes infinite and trigonometric floats
 	 *
 	 * @param float $data
 	 *
-	 * @return string|double
+	 * @return string|float
 	 */
-	private function normalizeFloat($data) {
+	private function normalizeFloat(float $data) {
 		if (is_infinite($data)) {
 			$postfix = 'INF';
 			if ($data < 0) {
 				$postfix = '-' . $postfix;
 			}
-			$data = $postfix;
-		} else {
-			if (is_nan($data)) {
-				$data = 'NaN';
-			}
+			return $postfix;
+		}
+
+		if (is_nan($data)) {
+			return 'NaN';
 		}
 
 		return $data;
@@ -181,12 +178,12 @@ class Normalizer {
 	 *
 	 * @used-by Nextcloud\LogNormalizer\Normalizer::normalize
 	 *
-	 * @param $data
+	 * @param mixed $data
 	 * @param int $depth
 	 *
 	 * @return array|null
 	 */
-	private function normalizeTraversable($data, $depth = 0) {
+	private function normalizeTraversable($data, int $depth = 0): ?array {
 		if (is_array($data) || $data instanceof \Traversable) {
 			return $this->normalizeTraversableElement($data, $depth);
 		}
@@ -197,12 +194,12 @@ class Normalizer {
 	/**
 	 * Converts each element of a traversable variable to String
 	 *
-	 * @param $data
+	 * @param mixed $data
 	 * @param int $depth
 	 *
 	 * @return array
 	 */
-	private function normalizeTraversableElement($data, $depth) {
+	private function normalizeTraversableElement($data, int $depth): array {
 		$maxObjectRecursion = $this->maxRecursionDepth;
 		$maxArrayItems = $this->maxArrayItems;
 		$count = 1;
@@ -210,8 +207,8 @@ class Normalizer {
 		$nextDepth = $depth + 1;
 		foreach ($data as $key => $value) {
 			if ($count >= $maxArrayItems) {
-				$normalized['...'] =
-					'Over ' . $maxArrayItems . ' items, aborting normalization';
+				$normalized['...']
+					= 'Over ' . $maxArrayItems . ' items, aborting normalization';
 				break;
 			}
 			$count++;
@@ -232,8 +229,8 @@ class Normalizer {
 	 *
 	 * @return null|string
 	 */
-	private function normalizeDate($data) {
-		if ($data instanceof \DateTime) {
+	private function normalizeDate($data): ?string {
+		if ($data instanceof \DateTimeInterface) {
 			return $data->format($this->dateFormat);
 		}
 
@@ -250,9 +247,9 @@ class Normalizer {
 	 * @param mixed $data
 	 * @param int $depth
 	 *
-	 * @return array|null
+	 * @return mixed[]|string|null
 	 */
-	private function normalizeObject($data, $depth) {
+	private function normalizeObject($data, int $depth) {
 		if (is_object($data)) {
 			if ($data instanceof Throwable) {
 				return $this->normalizeException($data);
@@ -275,18 +272,18 @@ class Normalizer {
 	}
 
 	/**
-	 * Converts an Exception to String
+	 * Converts an Exception to a string array
 	 *
 	 * @param Throwable $exception
 	 *
-	 * @return string[]
+	 * @return mixed[]
 	 */
-	private function normalizeException(Throwable $exception) {
+	private function normalizeException(Throwable $exception): array {
 		$data = [
-			'class'   => get_class($exception),
+			'class' => get_class($exception),
 			'message' => $exception->getMessage(),
-			'code'    => $exception->getCode(),
-			'file'    => $exception->getFile() . ':' . $exception->getLine(),
+			'code' => $exception->getCode(),
+			'file' => $exception->getFile() . ':' . $exception->getLine(),
 		];
 		$trace = $exception->getTraceAsString();
 		$data['trace'] = $trace;
@@ -302,29 +299,28 @@ class Normalizer {
 	/**
 	 * Formats the output of the object parsing
 	 *
-	 * @param $object
+	 * @param object $object
 	 *
 	 * @return string
 	 */
-	private function getObjetName($object) {
+	private function getObjetName(object $object): string {
 		return sprintf('[object] (%s)', get_class($object));
 	}
 
 	/**
-	 * Converts a resource to a String
+	 * Converts a resource to a string
 	 *
 	 * @used-by Nextcloud\LogNormalizer\Normalizer::normalize
 	 *
-	 * @param $data
+	 * @param resource|mixed $data
 	 *
 	 * @return string|null
 	 */
-	private function normalizeResource($data) {
+	private function normalizeResource($data): ?string {
 		if (is_resource($data)) {
 			return '[resource] ' . substr((string)$data, 0, 40);
 		}
 
 		return null;
 	}
-
 }
